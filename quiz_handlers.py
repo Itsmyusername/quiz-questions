c64716ebbdf0491447a7b49085887726674fed12
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-04-06 12:26:40 +0300

    Update quiz_handlers.py

--- quiz_handlers.py
+++ quiz_handlers.py
@@ -2,22 +2,8 @@ import re
 
 
 def dictify_questions(text: str):
-
     questions = re.findall(r"Вопрос \d+:\s(.*?)\sОтвет", text, re.DOTALL)
     answers = re.findall(r"Ответ:\s(.*?)\.\s\s", text)
 
     questionnaire = list(zip(questions, answers))
-
     return questionnaire
-
-
-def main():
-    with open("QA/123.txt", encoding="KOI8-R") as f:
-        whole_text = f.read()
-
-    questionnaire = dictify_questions(whole_text)
-    print(questionnaire)
-
-
-if __name__ == '__main__':
-    main()
